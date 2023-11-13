0ecd8950d21bfd62ba4b74ce94c42b9e528081a6
August Fors <augustjf@stud.ntnu.no>
2023-11-13 15:20:54 +0100

    initial Commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtle_exploration'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='august',
+    maintainer_email='augustjf@stud.ntnu.no',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'turtle_exploration = turtle_exploration.turtle_exploration:main'
+        ],
+    },
+)
---
+++ turtle_exploration/turtle_exploration.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from turtle_exploration.')
+
+
+if __name__ == '__main__':
+    main()
